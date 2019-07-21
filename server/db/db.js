43d2bf8381918d71e91f374daa2a2a35260e21c2
Bryan <bryan_stevens314@yahoo.com>
2019-07-21 01:00:43 -0700

    Updated datbase url

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-  'postgres://localhost:5432/stackchat', {
+  'postgres://qiwmdbxvaumfcf:77f65e9fd4eecdf4e953afbcc68ae59ed40068611b38c0c217b39facb42ba439@ec2-107-20-173-2.compute-1.amazonaws.com:5432/d6pth6ebaf7lc8', {
     logging: false
   }
 );
